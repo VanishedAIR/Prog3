064affbe1db7918a4a969406014fe3be8856e6cd
Vansh Patel <vansh@Mac.lan>
2025-03-02 16:10:11 -0800

    t1

---
+++ mysort.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int A[], int size) {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < (size - 1) - i; j++) {
+            if (A[j] > A[j + 1]) {
+                int temp = A[j];
+                A[j] = A[j + 1];
+                A[j + 1] = temp;
+            }
+        }
+    }
+}
+
